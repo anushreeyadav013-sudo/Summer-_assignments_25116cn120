08a8c8f161fa92f7cd440dfe83082cd5f85c35f3
anushreeyadav013-sudo <anushreeyadav013@gmail.com>
2026-06-27 11:04:27 -0700

    Add files via upload

---
+++ DAY-24/day24-96.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ int main(){
+    char str[100];
+    int i,j;
+    printf("enter a string");
+    scanf("%s",str);
+    for(i=0; str[i]!='\0'; i++){
+        for(j=i+1; str[j]!='\0';){
+            if(str[i]==str[j]){
+                int k;
+                for(k=j; str[k]!='\0'; k++)
+                str[k]=str[k+1];
+            }else{
+                j++;
+            }
+        }
+    }
+    printf("After removing duplicates:%s",str);
+    return 0;
+ }
